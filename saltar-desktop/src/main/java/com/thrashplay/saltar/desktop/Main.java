08f437217ec727ab399741b34fbafd41d1db986b
skleinjung <skleinjung@gmail.com>
2016-03-11 06:03:55 -0600

    Began creating desktop implementation of Luna.

---
+++ saltar-desktop/src/main/java/com/thrashplay/saltar/desktop/Main.java
@@ -0,0 +1,14 @@
+package com.thrashplay.saltar.desktop;
+
+import com.thrashplay.luna.desktop.LunaWindow;
+
+/**
+ * TODO: Add class documentation
+ *
+ * @author Sean Kleinjung
+ */
+public class Main {
+    public static void main(String[] args) {
+        new LunaWindow("Saltar", 640, 480);
+    }
+}
