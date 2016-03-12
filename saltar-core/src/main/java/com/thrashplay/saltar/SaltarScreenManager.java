8305ac30f9f483ae4e08db2addce4e091614f65a
skleinjung <skleinjung@gmail.com>
2016-03-12 02:53:30 -0600

    Implemented desktop versions of touch manager and sound manager. Wired up desktop version of Jounce.

--- saltar-core/src/main/java/com/thrashplay/saltar/SaltarScreenManager.java
+++ saltar-core/src/main/java/com/thrashplay/saltar/SaltarScreenManager.java
@@ -1,24 +1,14 @@
 package com.thrashplay.saltar;
 
-import com.thrashplay.luna.api.engine.Screen;
-import com.thrashplay.luna.api.engine.ScreenManager;
-import com.thrashplay.luna.api.graphics.Graphics;
+import com.thrashplay.luna.engine.DefaultScreenManager;
 
 /**
  * TODO: Add class documentation
  *
  * @author Sean Kleinjung
  */
-public class SaltarScreenManager implements ScreenManager {
-    private Screen currentScreen = new TestScreen();
-
-    @Override
-    public Screen getCurrentScreen() {
-        return currentScreen;
-    }
-
-    @Override
-    public void update(Graphics graphics) {
-
+public class SaltarScreenManager extends DefaultScreenManager {
+    public SaltarScreenManager() {
+        registerScreen("test", new TestScreen());
     }
 }
--- saltar-desktop/src/main/java/com/thrashplay/saltar/desktop/Main.java
+++ saltar-desktop/src/main/java/com/thrashplay/saltar/desktop/Main.java
@@ -1,6 +1,8 @@
 package com.thrashplay.saltar.desktop;
 
+import com.thrashplay.luna.desktop.LunaCanvas;
 import com.thrashplay.luna.desktop.LunaWindow;
+import com.thrashplay.luna.engine.LunaGameConfig;
 import com.thrashplay.saltar.SaltarScreenManager;
 
 /**
@@ -10,6 +12,15 @@ import com.thrashplay.saltar.SaltarScreenManager;
  */
 public class Main {
     public static void main(String[] args) {
-        new LunaWindow("Saltar", new SaltarScreenManager(), 640, 480);
+        new LunaWindow("Saltar", 640, 480) {
+            @Override
+            protected LunaGameConfig createGameConfig(LunaCanvas canvas) {
+                LunaGameConfig gameConfig = new LunaGameConfig();
+                gameConfig.setScreenManager(new SaltarScreenManager());
+                gameConfig.setDefaultScreen("test");
+                return gameConfig;
+            }
+        };
     }
 }
+
