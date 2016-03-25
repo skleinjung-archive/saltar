b5c818538b5ec4ec7f12b0bfb1886520911d7b75
skleinjung <skleinjung@gmail.com>
2016-03-25 16:12:54 -0500

    Created a system to allow the bounding boxes to change with each frame of a sprite's animations.

--- saltar-core/src/main/java/com/thrashplay/saltar/SaltarLevelScreen.java
+++ saltar-core/src/main/java/com/thrashplay/saltar/SaltarLevelScreen.java
@@ -1,9 +1,11 @@
 package com.thrashplay.saltar;
 
+import com.thrashplay.luna.animation.AnimationStateBasedRenderer;
 import com.thrashplay.luna.api.actor.ActorManager;
 import com.thrashplay.luna.api.actor.config.ActorConfig;
+import com.thrashplay.luna.api.animation.AnimationConfig;
 import com.thrashplay.luna.api.animation.AnimationRenderer;
-import com.thrashplay.luna.api.collision.CollisionHandler;
+import com.thrashplay.luna.api.collision.CrossCollisionDetector;
 import com.thrashplay.luna.api.component.*;
 import com.thrashplay.luna.api.engine.DefaultScreen;
 import com.thrashplay.luna.api.engine.GameObject;
@@ -12,21 +14,24 @@ import com.thrashplay.luna.api.geom.Rectangle;
 import com.thrashplay.luna.api.graphics.AnimationConfigManager;
 import com.thrashplay.luna.api.graphics.ImageManager;
 import com.thrashplay.luna.api.graphics.SpriteSheet;
-import com.thrashplay.luna.api.animation.AnimationConfig;
 import com.thrashplay.luna.api.input.InputManager;
 import com.thrashplay.luna.api.input.KeyCode;
 import com.thrashplay.luna.api.input.MultiTouchManager;
 import com.thrashplay.luna.api.level.LevelManager;
 import com.thrashplay.luna.api.level.config.LevelConfig;
-import com.thrashplay.luna.api.collision.CrossCollisionDetector;
 import com.thrashplay.luna.api.ui.Button;
+import com.thrashplay.luna.collision.BoundingBoxesDebugRenderer;
+import com.thrashplay.luna.collision.CrossBoundingBoxes;
+import com.thrashplay.luna.collision.RendererBasedBoundingBoxes;
 import com.thrashplay.luna.engine.LegacyGameObjectAdapter;
 import com.thrashplay.luna.input.VirtualKeyboard;
-import com.thrashplay.luna.animation.AnimationStateBasedRenderer;
 import com.thrashplay.luna.renderable.ClearScreen;
 import com.thrashplay.luna.renderable.FpsDisplay;
 import com.thrashplay.luna.ui.TextButton;
-import com.thrashplay.saltar.component.*;
+import com.thrashplay.saltar.component.KeyboardMovementController;
+import com.thrashplay.saltar.component.Player;
+import com.thrashplay.saltar.component.PlayerCollisionHandler;
+import com.thrashplay.saltar.component.ViewportScrollController;
 
 import java.util.List;
 
@@ -113,6 +118,11 @@ public class SaltarLevelScreen extends DefaultScreen {
         fpsDisplay.setRenderLayer(GameObject.RenderLayer.Overlay);
         gameObjectManager.register(fpsDisplay);
         gameObjectManager.register(new LegacyGameObjectAdapter("collision detector", new CrossCollisionDetector(gameObjectManager)));
+
+        GameObject debug = new GameObject("debug");
+        debug.setRenderLayer(GameObject.RenderLayer.Foreground);
+        debug.addComponent(new BoundingBoxesDebugRenderer(gameObjectManager));
+        gameObjectManager.register(debug);
     }
 
     private GameObject createPlayer(int startX, int startY) {
@@ -121,9 +131,9 @@ public class SaltarLevelScreen extends DefaultScreen {
         AnimationConfig deathAnimation = animationConfigManager.getAnimationConfig("animations/player/sara_death.json");
         SpriteSheet playerAnimationSpriteSheet = imageManager.createSpriteSheet(walkAnimation.getSpriteSheet());
 
-        ImageRenderer idleLeftImage = new ImageRenderer(playerAnimationSpriteSheet.getImage(1), true);
+        ImageRenderer idleLeftImage = new SpriteSheetImageRenderer(playerAnimationSpriteSheet, 1, true);
         idleLeftImage.setFlipHorizontally(true);
-        ImageRenderer idleRightImage = new ImageRenderer(playerAnimationSpriteSheet.getImage(1), true);
+        ImageRenderer idleRightImage = new SpriteSheetImageRenderer(playerAnimationSpriteSheet, 1, true);
         AnimationRenderer walkingLeftAnimation = new AnimationRenderer(walkAnimation, playerAnimationSpriteSheet);
         walkingLeftAnimation.setFlipHorizontally(true);
         AnimationRenderer walkingRightAnimation = new AnimationRenderer(walkAnimation, playerAnimationSpriteSheet);
@@ -150,8 +160,9 @@ public class SaltarLevelScreen extends DefaultScreen {
         player.addComponent(new Movement());
         player.addComponent(new Gravity(3));
         player.addComponent(new Player());
-        player.addComponent(new Collider(1, true, new Rectangle(maxPlayerVelocity + 1, 0, 26 - ((maxPlayerVelocity + 1) * 2), 48),  new Rectangle(0, maxPlayerVelocity + 1, 26, 48 - ((maxPlayerVelocity + 1) * 2))));
-        player.addComponent(CollisionHandler.class, new PlayerCollisionHandler());
+        player.addComponent(new Collider(1, true));
+        player.addComponent(new CrossBoundingBoxes(new RendererBasedBoundingBoxes(), maxPlayerVelocity + 1, maxPlayerVelocity + 1));
+        player.addComponent(new PlayerCollisionHandler());
         player.addComponent(new KeyboardMovementController(inputManager));
         player.addComponent(playerRenderer);
 
--- saltar-core/src/main/java/com/thrashplay/saltar/TestScreen.java
+++ saltar-core/src/main/java/com/thrashplay/saltar/TestScreen.java
@@ -1,7 +1,9 @@
 package com.thrashplay.saltar;
 
+import com.thrashplay.luna.animation.AnimationStateBasedRenderer;
+import com.thrashplay.luna.api.animation.AnimationConfig;
 import com.thrashplay.luna.api.animation.AnimationRenderer;
-import com.thrashplay.luna.api.collision.CollisionHandler;
+import com.thrashplay.luna.api.collision.CrossCollisionDetector;
 import com.thrashplay.luna.api.component.*;
 import com.thrashplay.luna.api.engine.DefaultScreen;
 import com.thrashplay.luna.api.engine.GameObject;
@@ -11,20 +13,22 @@ import com.thrashplay.luna.api.graphics.AnimationConfigManager;
 import com.thrashplay.luna.api.graphics.ImageManager;
 import com.thrashplay.luna.api.graphics.LunaImage;
 import com.thrashplay.luna.api.graphics.SpriteSheet;
-import com.thrashplay.luna.api.animation.AnimationConfig;
 import com.thrashplay.luna.api.input.InputManager;
 import com.thrashplay.luna.api.input.KeyCode;
 import com.thrashplay.luna.api.input.MultiTouchManager;
 import com.thrashplay.luna.api.math.Random;
-import com.thrashplay.luna.api.collision.CrossCollisionDetector;
 import com.thrashplay.luna.api.ui.Button;
+import com.thrashplay.luna.collision.CrossBoundingBoxes;
+import com.thrashplay.luna.collision.StaticBoundingBoxes;
 import com.thrashplay.luna.engine.LegacyGameObjectAdapter;
 import com.thrashplay.luna.input.VirtualKeyboard;
-import com.thrashplay.luna.animation.AnimationStateBasedRenderer;
 import com.thrashplay.luna.renderable.ClearScreen;
 import com.thrashplay.luna.renderable.FpsDisplay;
 import com.thrashplay.luna.ui.TextButton;
-import com.thrashplay.saltar.component.*;
+import com.thrashplay.saltar.component.KeyboardMovementController;
+import com.thrashplay.saltar.component.Player;
+import com.thrashplay.saltar.component.PlayerCollisionHandler;
+import com.thrashplay.saltar.component.ViewportScrollController;
 
 /**
  * TODO: Add class documentation
@@ -76,8 +80,9 @@ public class TestScreen extends DefaultScreen {
         player.addComponent(new Movement());
         player.addComponent(new Gravity(3));
         player.addComponent(new Player());
-        player.addComponent(new Collider(1, true, new Rectangle(maxPlayerVelocity + 1, 0, 30 - ((maxPlayerVelocity + 1) * 2), 55),  new Rectangle(0, maxPlayerVelocity + 1, 30, 55 - ((maxPlayerVelocity + 1) * 2))));
-        player.addComponent(CollisionHandler.class, new PlayerCollisionHandler());
+        player.addComponent(new Collider(1, true));
+        player.addComponent(new CrossBoundingBoxes(new StaticBoundingBoxes(new Rectangle(0, 0, 26, 48)), maxPlayerVelocity + 1, maxPlayerVelocity + 1));
+        player.addComponent(new PlayerCollisionHandler());
         player.addComponent(new KeyboardMovementController(inputManager));
         player.addComponent(playerRenderer);
         gameObjectManager.register(player);
@@ -106,9 +111,9 @@ public class TestScreen extends DefaultScreen {
 
                     block.addComponent(new ImageRenderer(image, true));
 
-                    if (y == screenBounds.getBottom() - 64) {
-                        block.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
-                    }
+//                    if (y == screenBounds.getBottom() - 64) {
+//                        block.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//                    }
 
                     System.out.println("Created " + blockCount + " blocks");
                     gameObjectManager.register(block);
@@ -125,48 +130,48 @@ public class TestScreen extends DefaultScreen {
             block.addComponent(new Position(32 * tileColumn, y));
             block.addComponent(new ImageRenderer(spriteSheet.getImage(tileIndex), true));
 
-            if (tileColumn == 1) {
-                block.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
-            }
+//            if (tileColumn == 1) {
+//                block.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//            }
 
             gameObjectManager.register(block);
         }
 
         GameObject platform = new GameObject("ground-platform");
         platform.addComponent(new Position(1300, screenBounds.getBottom() - 64 - 32));
-        platform.addComponent(new Collider(2, false, new Rectangle(0, 0, 48, 32)));
+//        platform.addComponent(new Collider(2, false, new Rectangle(0, 0, 48, 32)));
         platform.addComponent(new ImageRenderer(spriteSheet.getImage(14), true));
         gameObjectManager.register(platform);
 
         GameObject platformLeft = new GameObject("air-platform-left");
         platformLeft.addComponent(new Position(400, screenBounds.getBottom() - 64 - 128));
-        platformLeft.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//        platformLeft.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
         platformLeft.addComponent(new ImageRenderer(image1, true));
         gameObjectManager.register(platformLeft);
         GameObject platformCenter = new GameObject("air-platform-center");
         platformCenter.addComponent(new Position(432, screenBounds.getBottom() - 64 - 128));
-        platformCenter.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//        platformCenter.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
         platformCenter.addComponent(new ImageRenderer(image1, true));
         gameObjectManager.register(platformCenter);
         GameObject platformRight = new GameObject("air-platform-right");
         platformRight.addComponent(new Position(464, screenBounds.getBottom() - 64 - 128));
-        platformRight.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//        platformRight.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
         platformRight.addComponent(new ImageRenderer(image1, true));
         gameObjectManager.register(platformRight);
 
         GameObject platformLeft2 = new GameObject("air-platform-left2");
         platformLeft2.addComponent(new Position(600, screenBounds.getBottom() - 64 - 96));
-        platformLeft2.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//        platformLeft2.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
         platformLeft2.addComponent(new ImageRenderer(image1, true));
         gameObjectManager.register(platformLeft2);
         GameObject platformCenter2 = new GameObject("air-platform-center2");
         platformCenter2.addComponent(new Position(632, screenBounds.getBottom() - 64 - 96));
-        platformCenter2.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//        platformCenter2.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
         platformCenter2.addComponent(new ImageRenderer(image1, true));
         gameObjectManager.register(platformCenter2);
         GameObject platformRight2 = new GameObject("air-platform-right2");
         platformRight2.addComponent(new Position(664, screenBounds.getBottom() - 64 - 96));
-        platformRight2.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
+//        platformRight2.addComponent(new Collider(2, false, new Rectangle(0, 0, 32, 32)));
         platformRight2.addComponent(new ImageRenderer(image1, true));
         gameObjectManager.register(platformRight2);
 
